e0a7f8001d048988c99a2de052f0b2a6209ef540
dollon <dollon2@web.de>
2016-11-22 22:25:28 +0100

    bumpUpdate(smallfixtothelastbuild)

---
+++ Source/Revolver_Rouge/Revolver_Rouge.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Revolver_Rouge.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Revolver_Rouge, "Revolver_Rouge" );
